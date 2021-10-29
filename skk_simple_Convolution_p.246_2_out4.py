4f3b9847e968fc25d03d30a1fe11171c79d25ba7
chanmin07 <cksals2589@naver.com>
2021-10-29 18:54:47 +0900

    study

---
+++ skk_simple_Convolution_p.246_2_out4.py
@@ -0,0 +1,26 @@
+import sys, os
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+import numpy as np
+from common.util import im2col
+
+N=2  # data 개수
+x=np.random.rand(N,3,8,8) # N개의 3channel 8X8 data
+print(x)
+print("###################################################")
+col=im2col(x,5,5,stride=1,pad=0)
+print(col)
+
+FN=3 # 필터 개수
+w=np.random.randint(4, size=75*FN) #필터한개당 임의수 0-3 까지 75개 생성, 합 3개
+W=w.reshape(3,3,5,5) #Filters  W=w.reshape(1,3,5,5)
+print(W)
+col_W=W.reshape(FN,-1).T
+print('col_w :', col_W.shape)
+
+print(col_W)
+out=np.dot(col,col_W)
+print('reshape 전 out 형상 :', out.shape)
+print(out)
+out=out.reshape(N,4,4,-1).transpose(0,3,1,2) #(2,3,4,4)
+print(out)
+print(out.shape)
